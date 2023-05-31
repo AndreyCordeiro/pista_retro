097be0d692e60c4f24c2b9791104cdf212fae465
a <a>
2023-05-30 21:38:57 -0300

    Adicionando contador de pontos

--- tela_jogo.py
+++ tela_jogo.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.font.init()
 
 largura = 1440
 altura = 800
@@ -11,8 +12,12 @@ pygame.display.set_icon(icone)
 tela = pygame.display.set_mode((largura, altura), pygame.RESIZABLE)
 pygame.display.set_caption("Pista Retro")
 
+pontos = 0
 
 def criar_tela():
+
+    global pontos
+
     tela.fill((0, 128, 0))
 
     pygame.draw.rect(tela, (0, 128, 0), (1, 0, 340, 880))
@@ -26,6 +31,26 @@ def criar_tela():
     for i, y in enumerate(posicoes_y):
         if y >= altura:
             posicoes_y[i] = 0
-        posicoes_y[i] += 3  # velocidade do carro * tempo (frame)
+            pontos += 1  # Incrementar o contador de pontos
+        posicoes_y[i] += 3  # Velocidade do carro * tempo (frame)
+
+    exibir_pontos()
+
+    pygame.display.update()
+
+
+def exibir_pontos():
+    fonte = pygame.font.SysFont(None, 36)
+    texto = fonte.render(str(pontos).zfill(6), True, (255, 255, 255))
+    tela.blit(texto, (1300, 20))
+
+
+while True:
+    criar_tela()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
 
     pygame.display.update()
