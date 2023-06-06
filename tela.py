3ccc112aa75d4236dfd4f0813f38aa3035241477
adrieliket <adrielikethin.dossantos@gmail.com>
2023-06-06 20:44:14 -0300

    criando o deslocamento da tela

--- tela.py
+++ tela.py
@@ -1,6 +1,13 @@
 class Tela: 
-    def __init__(self, largura, altura, posicoes_y, posicoes_x):
+    def __init__(self, largura, altura, posicoes_y, posicoes_x, tempo_inicial, 
+                 tempo_ultimo, tempo_decorrido, velocidade_pista, posicao_pista, deslocamento_pista):
         self.largura = largura                                
         self.altura = altura
         self.posicoes_y = posicoes_y
         self.posicoes_x = posicoes_x
+        self.tempo_inicial = tempo_inicial
+        self.tempo_ultimo = tempo_ultimo
+        self.tempo_decorrido = tempo_decorrido
+        self.velocidade_pista = velocidade_pista
+        self.posicao_pista = posicao_pista
+        self.deslocamento_pista = deslocamento_pista
--- tela_jogo.py
+++ tela_jogo.py
@@ -1,7 +1,8 @@
 import pygame, time
 from tela import Tela
 
-objTela = Tela(1440, 800, [0, 100, 200, 300, 400, 500, 600, 700, 800], [500, 700, 900])
+objTela = Tela(1440, 800, [0, 100, 200, 300, 400, 500, 600, 700, 800], [500, 700, 900], 
+               time.time(), time.time(), 0.0, 60.0, 0, 0)
 
 icone = pygame.image.load("./imagens/icone.jpg")
 pygame.display.set_icon(icone)
@@ -9,9 +10,7 @@ pygame.display.set_icon(icone)
 tela = pygame.display.set_mode(
     (objTela.largura, objTela.altura), pygame.RESIZABLE)
 pygame.display.set_caption("Pista Retro")
-fps = 60
-velocidade = 1
-tempo_inicial = time.time()
+
 def renderizar_tela():
     tela.fill((0, 128, 0))
 
@@ -19,9 +18,10 @@ def renderizar_tela():
     pygame.draw.rect(tela, (105, 105, 105), (300, 0, 800, 840))
     pygame.draw.rect(tela, (0, 128, 0), (1100, 0, 340, 840))
 
-    tempo_atual = time.time() - tempo_inicial
-    deslocamento = velocidade * tempo_atual * fps
-    
+    objTela.deslocamento_pista = objTela.velocidade_pista * objTela.tempo_decorrido
+    objTela.posicao_pista = objTela.posicao_pista + objTela.deslocamento_pista
+        
+
     for x in objTela.posicoes_x:
         for y in objTela.posicoes_y:
             pygame.draw.rect(tela, (255, 255, 255), (x, y, 5, 50))
@@ -29,6 +29,10 @@ def renderizar_tela():
     for i, y in enumerate(objTela.posicoes_y):
         if y >= objTela.altura:
             objTela.posicoes_y[i] = 0
-        objTela.posicoes_y[i] += deslocamento
+        objTela.posicoes_y[i] += objTela.deslocamento_pista
 
     pygame.display.update()
+    t = time.time()
+    objTela.tempo_decorrido = t - objTela.tempo_ultimo
+    objTela.tempo_ultimo = t
+
