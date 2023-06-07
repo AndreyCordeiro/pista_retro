e8d3cf39eb106ff65575fa63e7e5417c1c9499ac
adrieliket <adrielikethin.dossantos@gmail.com>
2023-06-06 22:00:01 -0300

    passando a renderização da tela para o objteto

--- init.py
+++ init.py
@@ -2,16 +2,19 @@ from sys import exit
 
 import pygame
 from pygame.locals import *
-
-import tela_jogo
+from tela import Tela
+import time
 
 
 def init():
     pygame.init()
+
+    objTela = Tela(1440, 800, [-100, 100, 200, 300, 400, 500, 600, 700, 800], [500, 700, 900], 
+               time.time(), 60.0)
 
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 exit()
-        tela_jogo.renderizar_tela()
+        objTela.renderizar_tela()
--- tela.py
+++ tela.py
@@ -1,13 +1,45 @@
+import pygame
+import time
+
 class Tela: 
-    def __init__(self, largura, altura, posicoes_y, posicoes_x, tempo_inicial, 
-                 tempo_ultimo, tempo_decorrido, velocidade_pista, posicao_pista, deslocamento_pista):
+    def __init__(self, largura, altura, posicoes_y, posicoes_x, tempo_inicial, velocidade_pista):
         self.largura = largura                                
         self.altura = altura
         self.posicoes_y = posicoes_y
         self.posicoes_x = posicoes_x
         self.tempo_inicial = tempo_inicial
-        self.tempo_ultimo = tempo_ultimo
-        self.tempo_decorrido = tempo_decorrido
+        self.tempo_ultimo = self.tempo_inicial
+        self.tempo_decorrido = 0.0
         self.velocidade_pista = velocidade_pista
-        self.posicao_pista = posicao_pista
-        self.deslocamento_pista = deslocamento_pista
+        self.posicao_pista = 0
+        self.deslocamento_pista = 0
+        self.tela = pygame.display.set_mode((self.largura, self.altura), pygame.RESIZABLE)
+        self.icone = pygame.image.load("./imagens/icone.jpg")
+        self.tela.fill((0, 128, 0))
+        pygame.display.set_icon(self.icone)
+
+        pygame.display.set_caption("Pista Retro")
+
+
+    def renderizar_tela(self):
+
+        pygame.draw.rect(self.tela, (0, 128, 0), (1, 0, 340, 880))
+        pygame.draw.rect(self.tela, (105, 105, 105), (300, 0, 800, 840))
+        pygame.draw.rect(self.tela, (0, 128, 0), (1100, 0, 340, 840))
+
+        self.deslocamento_pista = self.velocidade_pista * self.tempo_decorrido
+        self.posicao_pista += self.deslocamento_pista
+
+        for x in self.posicoes_x:
+            for y in self.posicoes_y:
+                pygame.draw.rect(self.tela, (255, 255, 255), (x, y, 5, 50))
+
+        for i, y in enumerate(self.posicoes_y):
+            if y >= self.altura:
+                self.posicoes_y[i] = 0
+            self.posicoes_y[i] += self.deslocamento_pista
+
+        pygame.display.update()
+        t = time.time()
+        self.tempo_decorrido = t - self.tempo_ultimo
+        self.tempo_ultimo = t
