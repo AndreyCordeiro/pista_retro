9f4ea43ee26082531e4b7cf65ed2ada8965df995
Matheus Magalhaes <matheusurbano2002@hotmail.com>
2023-07-04 21:22:54 -0300

    Limites para cima e para baixo

--- carro_em_movimento.py
+++ carro_em_movimento.py
@@ -15,10 +15,21 @@ class CarroEmMovimento:
 
         if comando[pygame.K_RIGHT] or comando[pygame.K_d]:
             self.x += self.v
-            if (self.x > 1010):
+            if self.x > 1010:
                 self.x = 1010
 
         if comando[pygame.K_LEFT] or comando[pygame.K_a]:
             self.x -= self.v
-            if (self.x < 280):
+            if self.x < 280:
                 self.x = 280
+
+        if comando[pygame.K_UP] or comando[pygame.K_w]:
+            self.y -= self.v
+            if self.y < 0:
+                self.y = 0
+
+        if comando[pygame.K_DOWN] or comando[pygame.K_s]:
+            self.y += self.v
+            if self.y > 640:
+                self.y = 640
+
